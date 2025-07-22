5c343f5d32a9711bcf2c1a4b78c3b04e6f8a108b
arish03-hash <80570146+arish03-hash@users.noreply.github.com>
2025-07-22 13:35:39 +0100

    Time: 49 ms (83.67%), Space: 56.8 MB (92.9%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        int maxScore = 0, currScore = 0;
-
-        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] prefixSum = new int[nums.length + 1];
+        int maxScore = 0;
         for(int l = 0, r = 0; r<nums.length; r++){
-            while(!set.add(nums[r])){
-                currScore -= nums[l];
-                set.remove(nums[l++]);
+            prefixSum[r+1] = prefixSum[r] + nums[r];
+            if(map.containsKey(nums[r])){
+                l = Math.max(l, map.get(nums[r]) + 1);
             }
-            currScore += nums[r];
-            maxScore = Math.max(currScore, maxScore);
+            maxScore = Math.max(maxScore, prefixSum[r+1] - prefixSum[l]);
+            map.put(nums[r], r);
         }
         return maxScore;
     }
