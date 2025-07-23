d57578144dfae56b48db2d2fee23b604a715f93d
arish03-hash <80570146+arish03-hash@users.noreply.github.com>
2025-07-23 16:35:53 +0100

    Time: 581 ms (9%), Space: 45.2 MB (40.4%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,29 +1,34 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
-        Arrays.sort(nums);
-        recurPermute(nums,ans,res,freq);
+        recurPermute(0,nums,res);
         return res;
     }
 
-    public void recurPermute(int[] nums,List<Integer> ans,List<List<Integer>> res, boolean[] freq){
-        if(ans.size()==nums.length){
-            res.add(new ArrayList<>(ans));
+    public void recurPermute(int ind,int[] nums,List<List<Integer>> res){
+        if(ind==nums.length){
+            List<Integer> ans = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ans.add(nums[i]);
+            }
+            if(!res.contains(ans))
+                res.add(new ArrayList<>(ans));
             return;
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]  =true;
-                ans.add(nums[i]);
-                recurPermute(nums,ans,res,freq);
-                ans.remove(ans.size()-1);
-                freq[i] = false;
-                while(i+1 < nums.length && nums[i] == nums[i+1])
-                    i++;
-            }
-        }
+        for(int i=ind;i<nums.length;i++){
+                swap(i,ind,nums);
+                recurPermute(ind+1,nums,res);
+                swap(i,ind,nums);
+
+        }  
+    }
+    private void swap(int i, int j, int[] nums){
+        // nums[i] = nums[i] ^ nums[j];
+        // nums[j] = nums[i] ^ nums[j];
+        // nums[i] = nums[i] ^ nums[j];
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
