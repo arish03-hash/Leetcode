c7fd0a5303305308b284eedd492dc5205ecd3f9d
arish03-hash <80570146+arish03-hash@users.noreply.github.com>
2025-07-23 14:02:47 +0100

    Time: 3383 ms (5.48%), Space: 45.4 MB (87.67%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        
+        int aCount = 0;
+        int bCount = 0;
+        int lesser = Math.min(x, y);
+        int result = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 'b') {
+                result += Math.min(aCount, bCount) * lesser;
+                aCount = 0;
+                bCount = 0;
+            } else if (c == 'a') {
+                if (x < y && bCount > 0) {
+                    bCount--;
+                    result += y;
+                } else {
+                    aCount++;
+                }
+            } else {
+                if (x > y && aCount > 0) {
+                    aCount--;
+                    result += x;
+                } else {
+                    bCount++;
+                };
+            }
+            System.out.println(result);
+        }
+        
+        result += Math.min(aCount, bCount) * lesser;
+        
+        return result;
+    }
+}
