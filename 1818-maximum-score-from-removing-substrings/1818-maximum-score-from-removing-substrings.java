8ce6b07ba53b3e1e7863054c96d7b453eb131475
arish03-hash <80570146+arish03-hash@users.noreply.github.com>
2025-07-23 14:14:49 +0100

    Time: 56 ms (82.88%), Space: 45.8 MB (61.64%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -1,37 +1,40 @@
 class Solution {
     public int maximumGain(String s, int x, int y) {
-        
-        int aCount = 0;
-        int bCount = 0;
-        int lesser = Math.min(x, y);
-        int result = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c > 'b') {
-                result += Math.min(aCount, bCount) * lesser;
-                aCount = 0;
-                bCount = 0;
-            } else if (c == 'a') {
-                if (x < y && bCount > 0) {
-                    bCount--;
-                    result += y;
-                } else {
-                    aCount++;
-                }
-            } else {
-                if (x > y && aCount > 0) {
-                    aCount--;
-                    result += x;
-                } else {
-                    bCount++;
-                };
-            }
-            System.out.println(result);
+        int res = 0;
+        String top, bot;
+        int top_score, bot_score;
+
+        if(y>x){
+            top = "ba";
+            top_score = y;
+            bot = "ab";
+            bot_score = x;
         }
-        
-        result += Math.min(aCount, bCount) * lesser;
-        
-        return result;
+        else{
+            top=  "ab";
+            top_score = x;
+            bot = "ba";
+            bot_score = y;
+        }
+
+        StringBuilder stack = new StringBuilder();
+        for(char ch: s.toCharArray()){
+            if(ch == top.charAt(1) && stack.length() > 0 && stack.charAt(stack.length() - 1) == top.charAt(0)){
+                res += top_score;
+                stack.setLength(stack.length() - 1);
+            }
+            else
+                stack.append(ch);
+        }
+        StringBuilder newStack = new StringBuilder();
+        for(char ch: stack.toString().toCharArray()){
+            if(ch == bot.charAt(1) && newStack.length() > 0 && newStack.charAt(newStack.length() - 1) == bot.charAt(0)){
+                res += bot_score;
+                newStack.setLength(newStack.length() - 1);
+            }
+            else
+                newStack.append(ch);
+        }
+        return res;
     }
 }
