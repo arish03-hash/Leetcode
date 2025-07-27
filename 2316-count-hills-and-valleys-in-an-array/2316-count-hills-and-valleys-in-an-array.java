82c7d7e8802e337a5b9e44eccf701b3a5db1dfa9
arish03-hash <80570146+arish03-hash@users.noreply.github.com>
2025-07-27 21:37:11 +0100

    Time: 0 ms (100%), Space: 41.3 MB (97.55%) - LeetHub

---
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countHillValley(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        int left = 0;
+        for(int i = 1; i<n-1; i++){
+            if(nums[i] != nums[i+1]){
+                if((nums[i] > nums[left] && nums[i] > nums[i+1]) || (nums[i] < nums[left] && nums[i] < nums[i+1]))
+                    ans++;
+                left = i;
+            }
+        }
+        return ans;
+    }
+}
