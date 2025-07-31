2de069bd67902d81c360ec7113f94b3e10a9bb45
arish03-hash <80570146+arish03-hash@users.noreply.github.com>
2025-07-31 18:21:52 +0100

    Time: 37 ms (23.08%), Space: 180.5 MB (28.85%) - LeetHub

---
+++ 3290-number-of-subarrays-that-match-a-pattern-ii/3290-number-of-subarrays-that-match-a-pattern-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    private int[] kmp(String s){
+        int[] lps = new int[s.length()];
+        for(int i = 1; i<s.length(); i++){
+            int prevIdx = lps[i-1];
+            while(prevIdx > 0 && s.charAt(i) != s.charAt(prevIdx))
+                prevIdx = lps[prevIdx - 1];
+
+            lps[i] = prevIdx + (s.charAt(i) == s.charAt(prevIdx) ? 1:0);
+        }
+        return lps;
+    }
+    public int countMatchingSubarrays(int[] nums, int[] pattern) {
+        int n  =nums.length;
+        int m = pattern.length;
+        int ans = 0;
+        StringBuilder sString = new StringBuilder();
+        StringBuilder pString = new StringBuilder();
+
+        for(int i = 0; i<n-1; i++){
+            if(nums[i+1] > nums[i])
+                sString.append('a');
+            if(nums[i+1] == nums[i])
+                sString.append('s');
+            if(nums[i+1] < nums[i])
+                sString.append('d');
+        }
+        for(int i = 0; i<m; i++){
+            if(pattern[i] == 1)
+                pString.append('a');
+            if(pattern[i] == 0)
+                pString.append('s');
+            if(pattern[i] == -1)
+                pString.append('d');
+        }
+
+        String s = pString.toString() + "#" + sString.toString();
+        // System.out.println(s);
+        int[] kmpVec = kmp(s);
+        // for(int i  =0; i<kmpVec.length; i++)
+        //     System.out.print(kmpVec[i]);
+        for(int i = m; i<kmpVec.length; i++)
+            if(kmpVec[i] == m)
+                ans++;
+
+        return ans;
+    }
+}
