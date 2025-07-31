da78893fa6b4d918858b3b79d8065a55b391daff
arish03-hash <80570146+arish03-hash@users.noreply.github.com>
2025-07-31 17:59:13 +0100

    Time: 1 ms (100%), Space: 44.4 MB (75.57%) - LeetHub

---
+++ 3269-number-of-subarrays-that-match-a-pattern-i/3269-number-of-subarrays-that-match-a-pattern-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countMatchingSubarrays(int[] nums, int[] pattern) {
+        int n = nums.length;
+        int m = pattern.length;
+        int ans = 0;
+
+        for(int i = 0; i<=n-m; i++){
+            int flag = 1;
+            if(i + m >= n)
+                break;
+            for(int j = i; j-i < m && j + 1<n ;j++){
+                if((pattern[j - i] == -1) && (nums[j] > nums[j + 1]))
+                    continue;
+                if((pattern[j - i] == 0) && (nums[j] == nums[j + 1]))
+                    continue;
+                if((pattern[j - i] == 1) && (nums[j] < nums[j + 1]))
+                    continue;    
+                flag = 0;
+                break;
+            }
+            if(flag == 1)
+                ans++;
+        }
+        return ans;
+    }
+}
