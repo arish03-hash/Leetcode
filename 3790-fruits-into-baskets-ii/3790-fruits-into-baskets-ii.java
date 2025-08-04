f9b52a99b58bb30f6f9424c34d274cc966a6990d
arish03-hash <80570146+arish03-hash@users.noreply.github.com>
2025-08-04 17:50:52 +0100

    Time: 2 ms (61.12%), Space: 44.4 MB (61.76%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int ans = 0;
+        int n = fruits.length;
+        for(int i  = 0; i<n; i++){
+            for(int j=  0; j<n; j++){
+                if(fruits[i] <= baskets[j]){
+                    baskets[j] = -1;
+                    break;
+                }
+                if(j == n-1)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+}
